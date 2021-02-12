b2a49978dec4a21b5002c8869454159370c95304
Jennifer Fermin <jennifer.ferminp@gmail.com>
2021-02-12 11:43:58 -0300

    se agrega modal con AJAX

--- routes/rts.js
+++ routes/rts.js
@@ -14,13 +14,9 @@ router.post('/quotes', async (req, res) =>{
 		author: req.body.author,
 		quote: req.body.quote
 	});
-
 /* 	quote.push(req.body); */
 /* 	//2.recuperamos todas las citas
 	const quotes = await Quote.findAll(); */
-	
-	console.log('POST!!!!');
-	
 	//3. cargamos la vista
 	res.redirect('/quotes')
 });
@@ -30,14 +26,31 @@ router.get('/quotes', async (req, res) => {
 	console.log(quotes);
 	let mensaje = req.flash('mensaje') ;
   console.log(mensaje);
+	
 	res.render('quotes.ejs', {quotes: quotes, mensaje: mensaje});
 });
 
 router.get('/eliminar/:id', async (req,res) => {
   const quotte = await Quote.findByPk(req.params.id);
   await quotte.destroy();
-  req.flash('mensaje', `La cita fue eliminada.`);
+  //req.flash('mensaje', `La cita fue eliminada.`);
+  res.json({estado:true, mensaje:`La cita fue eliminada.`});
+});
+
+router.post('/editar/:id' , async (req,res) => {
+  let editar = await Quote.findOne(req.params.id);
+	await editar.update({where: {author: author, quote: quote}});
+  await editar.save();
+
+  req.flash('mensaje', `La cita fue editada.`);
   res.redirect("/quotes");
+});
+
+router.get('/editar/:id', async (req, res) => {
+  const quotte = await Quote.findByPk(req.params.id);  
+	let mensaje = req.flash('mensaje') ;
+  console.log(mensaje);
+  res.render('quotes.ejs', {quote: quote, mensaje: mensaje})
 });
 
 module.exports = router;
